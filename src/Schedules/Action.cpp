364fc9ceec5797a1a494fbd6b33e2c6b8c33b826
Dan Russell <danr94@gmail.com>
2016-03-05 23:35:06 -0800

    Min and max ranges for actions are now used to randomly calculate a range in between.

--- src/Schedules/Action.cpp
+++ src/Schedules/Action.cpp
@@ -2,12 +2,13 @@
 // Created by Dan on 2/6/2016.
 //
 
+#include <stdlib.h>
 #include "Action.h"
 
 double Action::calculateDuration() const
 {
-    // TODO: calculateRange using randomness.
-    return minDuration;
+    double random = (double)rand() / RAND_MAX;
+    return (minDuration + random * (maxDuration - minDuration));
 }
 
 ActionInstance* Action::createActionInstance(Operator* op) const
